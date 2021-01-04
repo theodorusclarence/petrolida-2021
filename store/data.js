b555c47ac49738927b1588e8867ec457256532a8
Theodorus Clarence <Clarence@Clarences-MacBook-Air.local>
2021-01-04 09:48:25 +0700

    added getstaticpath and prop example for competition/[name] removed some unused import

---
+++ store/data.js
@@ -0,0 +1,38 @@
+export const comps = [
+    {
+        route: 'oil-rig-design-competition',
+        img: 'ordc',
+        text: 'Oil Rig Design Competition',
+    },
+    {
+        route: 'fracturing-fluid-design-competition',
+        img: 'ffdc',
+        text: 'Fracturing Fluid Design Competition',
+    },
+
+    {
+        route: 'stock-trading-competition',
+        img: 'stc',
+        text: 'Stock Trading Competition',
+    },
+    {
+        route: 'petrosmart-competition',
+        img: 'petrosmart',
+        text: 'Petrosmart Competition',
+    },
+    {
+        route: 'case-study-competition',
+        img: 'csc',
+        text: 'Case Study Competition',
+    },
+    {
+        route: 'paper-competition',
+        img: 'paper',
+        text: 'Paper Competition',
+    },
+    {
+        route: 'business-case-competition',
+        img: 'bcc',
+        text: 'Business Case Competition',
+    },
+];
