ab7aa5a79cde5493acb69046b96b03fcbab3097c
Theodorus Clarence <Clarence@Clarences-MacBook-Air.local>
2021-01-06 23:31:31 +0700

    finished noncomp, added animation and static paths

--- store/data.js
+++ store/data.js
@@ -36,3 +36,30 @@ export const comps = [
         text: 'Business Case Competition',
     },
 ];
+
+export const noncomps = [
+    {
+        route: 'petrolida-talks',
+        img: 'pt',
+        title: 'Petrolida Talks 2021',
+        theme: 'Adapting to Consumer Behaviour During a Time of Crisis',
+        desc:
+            'Petrolida Talk 2021 is a talkshow that brings a new concept adapting to the new normal era. This event will be filled with speakers who are experienced and experts in their fields to discuss some current and important topics. Unlike other talk shows, by carrying out the hybrid concept between online and offline talkshows, the participants still have the opportunity to exchange ideas with the speakers while implementing health protocols. This talkshow is open to the public as an event that can provide information and education so that they can get inspiration to create ideas and works.',
+    },
+    {
+        route: 'city-tour',
+        img: 'ct',
+        title: 'City Tour',
+        theme: 'Adapting to Consumer Behaviour During a Time of Crisis',
+        desc:
+            'City Tour is one one of the series of Petrolida 2021’s post events, aiming to let the participants unwind after the competition by exploring the beauty of the city of heroes, Surabaya. Aside from that, Petrolida’s City Tour is also a perfect moment to connect everyone from fellow participants to the rest of the Petrolida 2021 committee.',
+    },
+    {
+        route: 'gala-dinner',
+        img: 'gd',
+        title: 'Gala Dinner',
+        theme: 'Adapting to Consumer Behaviour During a Time of Crisis',
+        desc:
+            'Gala dinner is a night of celebration to mark the finale of Petrolida 2021. Alongside with entertainments and exquisite dining, it is the perfect opportunity to engage and strengthen the relationship between the sponsors, judges, participants, and event committee. We also recognize the achievements of fellow participants and excellence of our competition winners by awarding them. All in all, gala dinner is a one night to remember.',
+    },
+];
