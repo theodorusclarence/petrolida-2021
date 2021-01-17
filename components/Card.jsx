f2cfaf75eca990b65909e2e5daa2f34135626eb9
Theodorus Clarence <Clarence@Clarences-MacBook-Air.local>
2021-01-17 13:51:43 +0700

    changed bcc text to closed, inserted invitation letter

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,12 +7,12 @@ export default function Card({ img, text, route, non, register }) {
     return (
         <figure className={`card-${non ? 'non' : 'comp'} relative`}>
             <img className='block w-full' src={`/img/${img}.jpg`} alt={text} loading='lazy' />
-            <figcaption className='absolute opacity-0 hover:opacity-100 transition-opacity bg-black bg-opacity-50 top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center'>
+            <figcaption className='absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center transition-opacity bg-black bg-opacity-50 opacity-0 hover:opacity-100'>
                 <h3 className='text-white'>{text}</h3>
                 {/* If on register, and the route is bcc so turn it into new tab link */}
                 {register && route === 'business-case-competition' ? (
-                    <NewTabBtn outline href={'https://forms.gle/KPTZouYjzCfZyLtV9'}>
-                        Register
+                    <NewTabBtn outline disabled href={'/'}>
+                        Closed
                     </NewTabBtn>
                 ) : (
                     <Button card outline href={`/${pathHref}/${route}`}>
